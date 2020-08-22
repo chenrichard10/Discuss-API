b6bbc9e49fa15afa8f0e9c37c2a61f59e56613fc
Richard Chen <chenpipi@live.com>
2020-08-21 22:00:20 -0400

    Basic setup

---
+++ discussAI/api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Document(models.Model):
+    name = models.CharField(max_length=200)
+    pdf = models.FileField()
+
+    def __str__(self):
+        return self.name
