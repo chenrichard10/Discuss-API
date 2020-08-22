550fb17731a28a29eba733e6da4f7818935b72b4
Richard Chen <chenpipi@live.com>
2020-08-22 13:56:09 -0400

    More database settings updated

--- discussAI/discussAI/settings.py
+++ discussAI/discussAI/settings.py
@@ -82,7 +82,17 @@ WSGI_APPLICATION = 'discussAI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.postgresql',
+    #    'NAME': 'd3250m1khfu9eu',
+    #    'USER': 'jqewrssfjgxyzx',
+    #    'PASSWORD': '5347312a0e3aac72ac1fffee19cd19215022d66bace4d68353ba7d2e5ef765ab',
+    #    'HOST': 'ec2-18-210-180-94.compute-1.amazonaws.com',
+    #    # Or an IP Address that your DB is hosted on
+    #    'PORT': '5432',
+    #}
+}
 
 
 # Password validation
