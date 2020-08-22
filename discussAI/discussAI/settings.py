26a63f415c47ea363b775fa4f9bb87d0eb1a37f6
Richard Chen <chenpipi@live.com>
2020-08-22 13:32:07 -0400

    Update settings.py

--- discussAI/discussAI/settings.py
+++ discussAI/discussAI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k+#6wkmnj#ar*y0_oqw0n74m2(%ojuqubid%i6k^shw3oopr*j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
